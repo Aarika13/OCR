b4fb725941dfc2db83aeb5897ab71a0ba291fff1
Sunil_Bhagat <sunil.bhagat@aspiresoftserv.com>
2023-03-17 19:25:13 +0530

    commit

--- f_app.py
+++ f_app.py
@@ -1,30 +1,30 @@
-import pandas as pd
-import streamlit as st
+# import pandas as pd
+# import streamlit as st
 
-# Cache the dataframe so it's only loaded once
-@st.cache_data
-def load_data():
-    return pd.DataFrame(
-        {
-            "id ": 1,
-            "bill_no." : 1,
-            "name" : "Minal",
-            "company" : "Aspire",
-            "Mobile Number" : 1234567890,
-            "E-mail" : "aar@gmail.com",
-            "Date" : 12/9/2012,
-            "Type_of_bill" : "",
-            "Quantity" : 1,
-            "Description" : "",
-            "Amount" : 4234567.00
-        }
-    ).rename_index()
+# # Cache the dataframe so it's only loaded once
+# @st.cache_data
+# def load_data():
+#     return pd.DataFrame(
+#         {
+#             "id ": 1,
+#             "bill_no." : 1,
+#             "name" : "Minal",
+#             "company" : "Aspire",
+#             "Mobile Number" : 1234567890,
+#             "E-mail" : "aar@gmail.com",
+#             "Date" : 12/9/2012,
+#             "Type_of_bill" : "",
+#             "Quantity" : 1,
+#             "Description" : "",
+#             "Amount" : 4234567.00
+#         }
+#     ).rename_index()
 
-# Boolean to resize the dataframe, stored as a session state variable
-st.checkbox("Use container width", value=False, key="use_container_width")
+# # Boolean to resize the dataframe, stored as a session state variable
+# st.checkbox("Use container width", value=False, key="use_container_width")
 
-df = load_data()
+# df = load_data()
 
-# Display the dataframe and allow the user to stretch the dataframe
-# across the full width of the container, based on the checkbox value
+# # Display the dataframe and allow the user to stretch the dataframe
+# # across the full width of the container, based on the checkbox value
 st.dataframe(df, use_container_width=st.session_state.use_container_width)
--- main.py
+++ main.py
@@ -1,46 +1,46 @@
-import cv2
-import pytesseract
-import os
-
-# specify the path to the folder containing the image files
-folder_path = "/home/sunil/Desktop/np/uploads"
-
-# loop through all the files in the folder
-for filename in os.listdir(folder_path):
-    # check if the file is an image file
-    if filename.endswith(".jpeg") or filename.endswith(".png") or filename.endswith(".jpg"):
-        # read the image file
-        print(filename)
-        img = cv2.imread(os.path.join(folder_path, filename))
-
-        # display the image
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-        # apply thresholding : used to differentiate the background and foreground
-        thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-
-        # recognize text with Pytesseract
-        text = pytesseract.image_to_string(thresh)
-
-        # print the recognized text
-
-print(text)
-
 # import cv2
 # import pytesseract
 # import os
 
-# # read the image file
-# img = cv2.imread(r'/home/sunil/Desktop/download.pdf')
+# # specify the path to the folder containing the image files
+# folder_path = "/home/sunil/Desktop/np/uploads"
 
-# # convert the image to grayscale
-# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# # loop through all the files in the folder
+# for filename in os.listdir(folder_path):
+#     # check if the file is an image file
+#     if filename.endswith(".jpeg") or filename.endswith(".png") or filename.endswith(".jpg"):
+#         # read the image file
+#         print(filename)
+#         img = cv2.imread(os.path.join(folder_path, filename))
 
-# # apply thresholding
-# thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+#         # display the image
+#         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# # recognize text with Pytesseract
-# text = pytesseract.image_to_string(thresh)
+#         # apply thresholding : used to differentiate the background and foreground
+#         thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
-# # print the recognized text
+#         # recognize text with Pytesseract
+#         text = pytesseract.image_to_string(thresh)
+
+#         # print the recognized text
+
 # print(text)
+
+import cv2
+import pytesseract
+import os
+
+# read the image file
+img = cv2.imread(r'/home/sunil/Desktop/np/uploads/car-service.png')
+
+# convert the image to grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# apply thresholding
+thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+# recognize text with Pytesseract
+text = pytesseract.image_to_string(thresh)
+
+# print the recognized text
+print(text)
